d651c6e7cacba81844b02b230db7f7d178825e5c
HerrHerting <dr.carsten.herting@emag-herzberg.de>
2025-12-08 08:28:05 +0100

    Update sketch.js

--- BrownTube/sketch.js
+++ BrownTube/sketch.js
@@ -1,11 +1,8 @@
-let sliderE, sliderB, sliderU, sliderI;
 let canvas;
-
+let w, h;
 let screen, vorn;
 
-let a;
-
-let w, h;
+let sliderE, sliderB, sliderU, sliderI, EBtn, BBtn;
 
 function preload()
 {
@@ -23,44 +20,27 @@ function setup()
   fill(0);
   noStroke();
 
-/*
-  let plusBtn = createButton('Down');
-  plusBtn.position(30, 71);
-  plusBtn.addClass('button');
-  plusBtn.mousePressed(() => a = 0);
-
-  let minusBtn = createButton('Up');
-  minusBtn.position(30, 121);
-  minusBtn.addClass('button');
-  minusBtn.mousePressed(() => a = 1);
-    
-  let clearBtn = createButton('Clear');
-  clearBtn.position(30, 171);
-  clearBtn.addClass('button');
-  clearBtn.mousePressed(() => a = 2);
-
-  toggle = createCheckbox('', false);
-  toggle.position(30, 226);
-  toggle.addClass('toggle');
-*/
-
   sliderI = createSlider(0, 3000, 0); // Beispielwerte
-  sliderI.position(450, 30); // unterhalb des Toggles
   sliderI.addClass('slider');
   
-  sliderU = createSlider(0, 5000, 0); // Beispielwerte
-  sliderU.position(450, 80); // unterhalb des Toggles
+  sliderU = createSlider(5, 5000, 5); // Beispielwerte
   sliderU.addClass('slider');
   
   sliderE = createSlider(-5000, 5000, 0); // Beispielwerte
-  sliderE.position(450, 130); // unterhalb des Toggles
   sliderE.addClass('slider');
 
   sliderB = createSlider(-10000, 10000, 0); // Beispielwerte
-  sliderB.position(450, 180); // unterhalb des Toggles
   sliderB.addClass('slider');
   
-  doDimensions();
+  EBtn = createButton('Zero');
+  EBtn.addClass('button');
+  EBtn.mousePressed(() => sliderE.value(0));
+
+  BBtn = createButton('Zero');
+  BBtn.addClass('button');
+  BBtn.mousePressed(() => sliderB.value(0));
+   
+  windowResized();
 }
 
 function windowResized()
@@ -75,6 +55,19 @@ function windowResized()
   }
   requestAnimationFrame(fixSize); // start resizing in next animation frame
   doDimensions();
+
+  sliderI.position(w*0.45, h*0.045);
+  sliderU.position(w*0.45, h*0.115);
+  sliderE.position(w*0.45, h*0.185);
+  sliderB.position(w*0.45, h*0.255);
+
+  sliderI.size(w*0.495);
+  sliderU.size(w*0.495);
+  sliderE.size(w*0.495);
+  sliderB.size(w*0.495);
+  
+  EBtn.position(w*0.44 - EBtn.size().width, h*0.185 - 13);
+  BBtn.position(w*0.44 - BBtn.size().width, h*0.255 - 13);
 }
 
 function doDimensions()
@@ -102,13 +95,13 @@ function draw()
 
   fill(255);
   
-  text("Heizstrom " + nf(sliderI.value() / 1000, 1, 1) + "A", 600, 30);
-  text("Beschleunigungsspannung "  + nf(sliderU.value(), 1, 0) + "V", 600, 80);
-  text("Ablenkspannung "  + nf(sliderE.value(), 1, 0) + "V", 600, 130);
-  text("Magnetfeld "  + nf(sliderB.value()/1000, 1, 2) + "mT", 600, 180);
+  text("Heizstrom " + nf(sliderI.value() / 1000, 1, 1) + "A", w*0.55, h*0.045);
+  text("Beschleunigungsspannung "  + nf(sliderU.value(), 1, 0) + "V", w*0.55, h*0.115);
+  text("Ablenkspannung "  + nf(sliderE.value(), 1, 0) + "V", w*0.55, h*0.185);
+  text("Magnetfeld "  + nf(sliderB.value()/1000, 1, 2) + "mT", w*0.55, h*0.255);
 
   textAlign(RIGHT, BOTTOM);
-  text("von C. Herting 2025", w-20, h-20); 
+  text("von C. Herting 2025", width-20, height-20); 
 }
 
 function mousePressed(ev)
@@ -125,7 +118,6 @@ function renderElectrons(anz, ub, uy, tesla)
 {
   let STEPSCALE = 500000; // für die Skalierung mit der Beschleunigungsspannung
 
-  ub += 0.5;
   strokeWeight(5); stroke(225,240,255,40);
 
   for(let i=0; i<anz; i++)
